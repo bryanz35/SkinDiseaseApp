3eebb1fe4d8dc7612615c980b7fd14df753527e8
bryanz35 <78311442+bryanz35@users.noreply.github.com>
2024-10-22 17:42:17 -0400

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'home.dart';
-import 'favorites.dart';
 import 'results.dart';
 void main() {
   runApp(MyApp());

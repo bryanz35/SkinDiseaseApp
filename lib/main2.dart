2e3f0ba2d95159856c5a6b109837fec1a12afb1a
bryanz35 <78311442+bryanz35@users.noreply.github.com>
2024-10-23 17:34:17 -0400

    cursed
    main2.dart - slightly fixed aru's code
    main.dart - very cursed half merged abomination. will fix camera later

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,198 +1,29 @@
-import 'dart:async';
 import 'dart:io';
 
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:camera/camera.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
 
-
-import 'results.dart';
-import 'camera.dart';
-
-List <CameraDescription> _cameras = <CameraDescription>[];
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  try {
-      WidgetsFlutterBinding.ensureInitialized();
-      _cameras = await availableCameras();
-    } on CameraException catch (e) {
-      print('Error: $e.code\nError Message: $e.message');
-    }
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade100),
-        ),
-        home: HomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TBD',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: SplashScreen(),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-}
-
-
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  int _currentIndex = 0;
-  late List<Widget> _pages;
-
-  @override
-  void initState() {
-    super.initState();
-    _pages = [
-      HomeScreen(),
-      ProfileScreen(),
-      MoreInfoScreen(),
-      ContactScreen(),
-      CameraScreen(
-        camera: camera,
-      ),
-    ];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TBD App'),
-      ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'More Info'),
-          BottomNavigationBarItem(icon: Icon(Icons.contact_phone), label: 'Contact'),
-          BottomNavigationBarItem(icon: Icon(Icons.camera), label: 'Camera'),
-        ],
-      ),
-    );
-    
-}
-
-  
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          showSurveyDialog(context);
-        },
-        style: ElevatedButton.styleFrom(
-          shape: CircleBorder(),
-          padding: EdgeInsets.all(50),
-          backgroundColor: Colors.blue,
-        ),
-        child: Icon(Icons.camera_alt, size: 50),
-      ),
-    );
-  }
-  void showSurveyDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("Survey"),
-          content: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text("Do you have a fever?"),
-                SurveyQuestion(),
-                SizedBox(height: 10),
-                Text("Is the rash spreading?"),
-                SurveyQuestion(),
-                SizedBox(height: 10),
-                Text("Do you feel pain or itchiness?"),
-                SurveyQuestion(),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text("Submit"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-
-class SettingsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings Screen'),
-    );
-  }
-}
-
-class CameraScreen extends StatelessWidget {
-  final CameraDescription camera;
-
-  CameraScreen({required this.camera});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Camera Example'),
-      ),
-      body: Center(
-        child: Text('Camera: ${camera.name}'),
-      ),
-    );
-  }
-}
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -238,7 +69,98 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeScreen(),
+    ProfileScreen(),
+    MoreInfoScreen(),
+    ContactScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TBD App'),
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'More Info'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contact_phone), label: 'Contact'),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showSurveyDialog(context);
+        },
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(50),
+          backgroundColor: Colors.blue,
+        ),
+        child: Icon(Icons.camera_alt, size: 50),
+      ),
+    );
+  }
+
+  void showSurveyDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Survey"),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text("Do you have a fever?"),
+                SurveyQuestion(),
+                SizedBox(height: 10),
+                Text("Is the rash spreading?"),
+                SurveyQuestion(),
+                SizedBox(height: 10),
+                Text("Do you feel pain or itchiness?"),
+                SurveyQuestion(),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Submit"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
 
 class SurveyQuestion extends StatefulWidget {
   @override

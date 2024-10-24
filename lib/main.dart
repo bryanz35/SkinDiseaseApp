cae788df9b766ab3ae66ec8c2319ae903f54fe45
AryahiT <tiwari26a@ncssm.edu>
2024-10-24 16:28:08 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,28 @@ import 'learn.dart';
 late List<CameraDescription> cameras;
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SkinSpot',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF0A4DA2), // Deep Blue
+        accentColor: Color(0xFF87CEEB),  // Light Blue
+        scaffoldBackgroundColor: Color(0xFFF3F4F6), // Light Grey
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Color(0xFF333333)), // Dark Grey
+        ),
+        buttonTheme: ButtonThemeData(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          buttonColor: Color(0xFF0A4DA2), // Deep Blue
+        ),
       ),
       home: SplashScreen(),
     );
@@ -30,8 +37,6 @@ class MyApp extends StatelessWidget {
 }
 
 class SplashScreen extends StatefulWidget {
-
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -50,14 +55,23 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFF0A4DA2),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("SkinSpot", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            Text(
+              "SkinSpot",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
             SizedBox(height: 20),
-            CircularProgressIndicator(),
+            CircularProgressIndicator(
+              color: Color(0xFF87CEEB),
+            ),
           ],
         ),
       ),
@@ -66,8 +80,6 @@ class _SplashScreenState extends State<SplashScreen> {
 }
 
 class MyHomePage extends StatefulWidget {
-
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -84,7 +96,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('SkinSpot', style: TextStyle(fontSize:  30, fontWeight: FontWeight.bold))),
+      appBar: AppBar(
+        title: Text('SkinSpot', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+        backgroundColor: Color(0xFF0A4DA2),
+      ),
       body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
@@ -93,6 +108,8 @@ class _MyHomePageState extends State<MyHomePage> {
             _currentIndex = index;
           });
         },
+        selectedItemColor: Color(0xFF0A4DA2),
+        unselectedItemColor: Colors.grey,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.camera), label: 'Identify'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
